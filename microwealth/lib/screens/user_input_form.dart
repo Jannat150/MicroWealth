ecb87be11e124df9cd1598cc07dd4924a9b7342d
Jannat <cshsjannat2987@gmail.com>
2026-03-09 15:27:23 +0530

    feat: implemented user input form with validation

---
+++ microwealth/lib/screens/user_input_form.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class UserInputForm extends StatefulWidget {
+  const UserInputForm({super.key});
+
+  @override
+  State<UserInputForm> createState() => _UserInputFormState();
+}
+
+class _UserInputFormState extends State<UserInputForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Input Form"),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+
+        child: Form(
+          key: _formKey,
+
+          child: Column(
+            children: [
+
+              // Name Field
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: "Name",
+                  border: OutlineInputBorder(),
+                ),
+
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your name";
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 20),
+
+              // Email Field
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+
+                keyboardType: TextInputType.emailAddress,
+
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your email";
+                  }
+
+                  if (!value.contains("@")) {
+                    return "Enter a valid email";
+                  }
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 25),
+
+              // Submit Button
+              ElevatedButton(
+
+                onPressed: () {
+
+                  if (_formKey.currentState!.validate()) {
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Form Submitted Successfully"),
+                      ),
+                    );
+
+                  }
+
+                },
+
+                child: const Text("Submit"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
